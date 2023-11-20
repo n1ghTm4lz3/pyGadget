5da822010563711b2517e511f3d15bbab983792a
No1lz <No1lz@protonmail.com>
2023-11-20 14:28:01 +0800

    [!] HxASCII -> Add ASCII to Hex

--- HxASCII.py
+++ HxASCII.py
@@ -2,32 +2,58 @@
 Title: HxASCII
 Author: n1ghTm4lz3
 Description:
-	Transform hexadecimal to ASCII
+	Transform hexadecimal to ASCII or ASCII to hexadecimal
 """
 
 def main():
 	while True:
-		cipher = input("Hex: ")
+		cipher = input("String: ")
 
 		if cipher == "":
 			exit()
 		else:
 			transform(cipher)
 
+def message():
+	print("+===============================================+")
+	print("|        Hexadecimal & ASCII Transformer        |")
+	print("|                                               |")
+	print("| Input Sample: 0x3456 / 0aAbdc                 |")
+	print("| Syntax:                                       |")
+	print("|   0x => Hex to ASCII                          |")
+	print("|   0a => ASCII to Hex                          |")
+	print("|   ?  => Help Message                          |")
+	print("+===============================================+")
+
+
 def transform(cipher):
-	plaintext = ""
+	ascii_plaintext = ""
+	hex_plaintext = ""
 	hxtemp = 0
 
-	for i in range(0, len(cipher), 2):
-		hxtemp = int(cipher[i:i+2], 16)
-		if hxtemp > 31 and hxtemp < 127:
-			plaintext += chr(hxtemp)
-		else:
-			plaintext += ' '
+	if cipher[0] == "?":
+		message()
+		return
+	elif cipher[0:2] == "0x":
+		for i in range(2, len(cipher), 2):
+			hxtemp = int(cipher[i:i+2], 16)
+			if hxtemp > 31 and hxtemp < 127:
+				ascii_plaintext += chr(hxtemp)
+			else:
+				ascii_plaintext += ' '
+			hex_plaintext += hex(hxtemp)[2:] + " "
+	elif cipher[0:2] == "0a":
+		ascii_plaintext = cipher[2:]
+		for i in range(2, len(cipher)):
+			hxtemp = ord(cipher[i])
+			hex_plaintext += hex(hxtemp)[2:] + " "
+		
 
-	print("ASCII: \"" + plaintext + "\"")
+	print("HEX: " + hex_plaintext)
+	print("ASCII: \"" + ascii_plaintext + "\"")
 	print()
 
 
 if __name__ == '__main__':
+	message()
 	main()
