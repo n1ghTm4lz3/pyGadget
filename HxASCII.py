fa3192c8b8bbdd307835e0e42a50588f6a477f0b
No1lz <No1lz@protonmail.com>
2023-12-01 10:34:39 +0800

    [!] HxASCII -> Add Decimal to ASCII and Hex

--- HxASCII.py
+++ HxASCII.py
@@ -18,10 +18,11 @@ def message():
 	print("+===============================================+")
 	print("|        Hexadecimal & ASCII Transformer        |")
 	print("|                                               |")
-	print("| Input Sample: 0x3456 / 0aAbdc                 |")
+	print("| Input Sample: 0x3456 / 0aAbdc / 0d080125      |")
 	print("| Syntax:                                       |")
 	print("|   0x => Hex to ASCII                          |")
 	print("|   0a => ASCII to Hex                          |")
+	print("|   0d => Decimal to ASCII and Hex              |")
 	print("|   ?  => Help Message                          |")
 	print("+===============================================+")
 
@@ -30,6 +31,7 @@ def transform(cipher):
 	ascii_plaintext = ""
 	hex_plaintext = ""
 	hxtemp = 0
+	dctemp = 0
 
 	if cipher[0] == "?":
 		message()
@@ -47,7 +49,15 @@ def transform(cipher):
 		for i in range(2, len(cipher)):
 			hxtemp = ord(cipher[i])
 			hex_plaintext += hex(hxtemp)[2:] + " "
-		
+	elif cipher[0:2] == "0d":
+		for i in range(2, len(cipher), 3):
+			dctemp = int(cipher[i:i+3])
+			hxtemp = int(hex(dctemp), 16)
+			if hxtemp > 31 and hxtemp < 127:
+				ascii_plaintext += chr(hxtemp)
+			else:
+				ascii_plaintext += ' '
+			hex_plaintext += hex(hxtemp)[2:] + " "
 
 	print("HEX: " + hex_plaintext)
 	print("ASCII: \"" + ascii_plaintext + "\"")
